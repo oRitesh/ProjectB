a40f578889f9f5bb954e5aaa320766d9779bd401
JakinHR <1128775@hr.nl>
2026-04-02 15:19:40 +0200

    info pagina + layout

--- ProjectB/Program.cs
+++ ProjectB/Program.cs
@@ -1,2 +1,81 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool running = true;
+
+while (running)
+{
+    Console.Clear();
+    Console.WriteLine("==================================");
+    Console.WriteLine("     WELKOM BIJ RESTAURANT B      ");
+    Console.WriteLine("==================================");
+    Console.WriteLine("1. Bekijk informatiepagina");
+    Console.WriteLine("2. Bekijk menukaart");
+    Console.WriteLine("3. Reserveer een tafel");
+    Console.WriteLine("0. Afsluiten");
+    Console.WriteLine();
+    Console.Write("Maak een keuze: ");
+
+    string? choice = Console.ReadLine();
+
+    switch (choice)
+    {
+        case "1":
+            ShowInformationPage();
+            break;
+        case "2":
+            ShowMenuPage();
+            break;
+        case "3":
+            ShowReservationPlaceholder();
+            break;
+        case "0":
+            running = false;
+            break;
+        default:
+            Console.WriteLine("Ongeldige keuze. Druk op een toets om verder te gaan...");
+            Console.ReadKey();
+            break;
+    }
+}
+
+static void ShowInformationPage()
+{
+    Console.Clear();
+    Console.WriteLine("==================================");
+    Console.WriteLine("         INFORMATIEPAGINA         ");
+    Console.WriteLine("==================================");
+    Console.WriteLine("Restaurantnaam : Restaurant B");
+    Console.WriteLine("Telefoonnummer : 0612345678");
+    Console.WriteLine("Adres          : Voorbeeldstraat 12, Amsterdam");
+    Console.WriteLine("Openingstijden : Dinsdag t/m zaterdag");
+    Console.WriteLine("                  17:00 - 00:00");
+    Console.WriteLine();
+    Console.WriteLine("Welkom bij Restaurant B!");
+    Console.WriteLine("Hier kunt u genieten van heerlijk eten");
+    Console.WriteLine("en binnenkort eenvoudig reserveren.");
+    Console.WriteLine();
+    Console.WriteLine("Druk op een toets om terug te gaan...");
+    Console.ReadKey();
+}
+
+static void ShowMenuPage()
+{
+    Console.Clear();
+    Console.WriteLine("==================================");
+    Console.WriteLine("            MENUKAART             ");
+    Console.WriteLine("==================================");
+    Console.WriteLine("Menu komt snel!");
+    Console.WriteLine();
+    Console.WriteLine("Druk op een toets om terug te gaan...");
+    Console.ReadKey();
+}
+
+static void ShowReservationPlaceholder()
+{
+    Console.Clear();
+    Console.WriteLine("==================================");
+    Console.WriteLine("           RESERVEREN             ");
+    Console.WriteLine("==================================");
+    Console.WriteLine("Reserveringssysteem komt binnenkort.");
+    Console.WriteLine();
+    Console.WriteLine("Druk op een toets om terug te gaan...");
+    Console.ReadKey();
+}
