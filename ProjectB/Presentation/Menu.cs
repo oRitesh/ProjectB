11cbf43f97413e1b93599e5f3ebde0978add6ec6
Stijn231106 <stijnvandermunnik1@gmail.com>
2026-05-12 18:05:57 +0200

    inlogsysteem rework en reservering veranderen fix

--- ProjectB/Presentation/Menu.cs
+++ ProjectB/Presentation/Menu.cs
@@ -1,6 +1,7 @@
 public static class Menu
 {
     private static Gebruiker? HuidigeGebruiker = new Gebruiker(0, 0, "gast", "", "", "");
+
     static void ShowInformationPage()
     {
         Console.Clear();
@@ -79,214 +80,84 @@ public static class Menu
                     break;
 
                 case "2":
-                    {
-                        DatabaseContext db = new DatabaseContext();
-                        ShowMenuUI menuUI = new ShowMenuUI(db);
-                        menuUI.ShowMenuPage();
-                        db.Close();
-                        break;
-                    }
+                {
+                    DatabaseContext db = new DatabaseContext();
+                    ShowMenuUI menuUI = new ShowMenuUI(db);
+                    menuUI.ShowMenuPage();
+                    db.Close();
+                    break;
+                }
 
                 case "3":
                     ShowReservationPage();
                     break;
 
                 case "4" when HuidigeGebruiker.Naam == "gast":
+                {
+                    DatabaseContext db = new DatabaseContext();
+                    UserAccess userAccess = new UserAccess(db);
+
+                    InlogUI inlogUI = new InlogUI(userAccess);
+                    RegistratieUI registratieUI = new RegistratieUI(userAccess);
+
+                    if (HuidigeGebruiker.ID == 0)
                     {
-                        DatabaseContext db = new DatabaseContext();
-                        UserAccess userAccess = new UserAccess(db);
-                        if (HuidigeGebruiker.ID == 0)
+                        Console.WriteLine("1. Inloggen");
+                        Console.WriteLine("2. Registreren");
+                        Console.WriteLine("0. Terug");
+                        Console.Write("Maak een keuze: ");
+                        string? loginChoice = Console.ReadLine();
+
+                        if (loginChoice == "1")
                         {
-                            Console.WriteLine("1. Inloggen");
-                            Console.WriteLine("2. Registreren");
-                            Console.WriteLine("0. Terug");
-                            Console.Write("Maak een keuze: ");
-                            string? loginChoice = Console.ReadLine();
-
-                            if (loginChoice == "1")
-                            {
-                                Console.Clear();
-                                Console.Write("E-mailadres: ");
-                                string? email = Console.ReadLine();
-                                Console.Write("Wachtwoord: ");
-                                string? pass = Console.ReadLine();
-
-                                var user = userAccess.GetUserByEmail(email, pass);
-                                if (user != null)
-                                {
-                                    HuidigeGebruiker = user;
-                                    Console.WriteLine($"Succesvol ingelogd! Welkom terug, {user.Naam}.");
-                                }
-                                else
-                                {
-                                    Console.WriteLine("Onjuiste gegevens.");
-                                }
-                                Console.ReadKey();
-                            }
-                            else if (loginChoice == "2")
-                            {
-                                string? regName = null;
-                                string? regEmail = null;
-                                string? regPhone = null;
-                                string? regPassword = null;
-
-                                bool userExists = false;
-
-                                while (string.IsNullOrWhiteSpace(regName))
-                                {
-                                    Console.Write("Voer uw naam in: ");
-                                    regName = Console.ReadLine();
-
-                                    if (string.IsNullOrWhiteSpace(regName))
-                                    {
-                                        Console.WriteLine("Naam mag niet leeg zijn. Probeer het opnieuw.");
-                                        Console.ReadKey();
-                                    }
-                                }
-
-                                while (string.IsNullOrWhiteSpace(regEmail))
-                                {
-                                    Console.Write("Voer uw e-mailadres in: ");
-                                    regEmail = Console.ReadLine();
-                                    if (string.IsNullOrWhiteSpace(regEmail))
-                                    {
-                                        Console.WriteLine();
-                                        Console.WriteLine("E-mailadres mag niet leeg zijn. Probeer het opnieuw.");
-                                        Console.ReadKey();
-                                    }
-
-                                    else if (!regEmail.Contains("@") || !regEmail.Contains("."))
-                                    {
-                                        Console.WriteLine();
-                                        Console.WriteLine("Ongeldig e-mailadres. Probeer het opnieuw.");
-                                        regEmail = null;
-                                        Console.ReadKey();
-                                    }
-
-                                    var checkUser = userAccess.GetUserByEmail(regEmail);
-                                    if (checkUser != null)
-                                    {
-                                        Console.WriteLine();
-                                        Console.WriteLine("E-mailadres is al in gebruik. Probeer het opnieuw.");
-                                        regEmail = null;
-                                        Console.ReadKey();
-                                    }
-                                }
-
-                                while (string.IsNullOrWhiteSpace(regPhone))
-                                {
-                                    Console.Write("Voer uw telefoonnummer in: ");
-                                    regPhone = Console.ReadLine();
-                                    if (string.IsNullOrWhiteSpace(regPhone))
-                                    {
-                                        Console.WriteLine();
-                                        Console.WriteLine("Telefoonnummer mag niet leeg zijn. Probeer het opnieuw.");
-                                        Console.ReadKey();
-                                    }
-
-                                    var checkUser = userAccess.GetUserByPhoneNumber(regPhone);
-                                    if (checkUser != null && checkUser.Rol == 1)
-                                    {
-
-                                        Console.WriteLine();
-                                        Console.WriteLine("Dit telefoonnummer is al gekoppeld aan een account.");
-                                        regPhone = null;
-                                        Console.ReadKey();
-                                    }
-
-                                    else if (checkUser != null && checkUser.Rol == 0)
-                                    {
-                                        userExists = true;
-                                    }
-                                }
-
-                                while (string.IsNullOrWhiteSpace(regPassword))
-                                {
-                                    Console.Write("Voer uw wachtwoord in: ");
-                                    regPassword = Console.ReadLine();
-                                    if (string.IsNullOrWhiteSpace(regPassword))
-                                    {
-                                        Console.WriteLine();
-                                        Console.WriteLine("Wachtwoord mag niet leeg zijn. Probeer het opnieuw.");
-                                        Console.ReadKey();
-                                    }
-
-                                    else if (regPassword.Length < 6)
-                                    {
-                                        Console.WriteLine();
-                                        Console.WriteLine("Wachtwoord moet minimaal 6 tekens bevatten. Probeer het opnieuw.");
-                                        Console.ReadKey();
-                                        regPassword = null; // Reset zodat de loop doorgaat
-                                    }
-
-                                }
-
-                                Console.Clear();
-                                Console.WriteLine("Registratie succesvol!");
-                                Console.WriteLine($"Welkom {regName}!");
-                                Console.WriteLine("\nDruk op een toets om verder te gaan...");
-                                Console.ReadKey();
-
-                                if (userExists)
-                                {
-                                    Console.WriteLine("We hebben uw gegevens gevonden van een eerdere reservering.");
-                                    Console.WriteLine("Uw gast-account wordt omgezet naar een officieel account...");
-                                    HuidigeGebruiker = userAccess.ChangeRole(regPhone, regName, 1, regEmail, regPassword);
-                                    Console.WriteLine("Account succesvol bijgewerkt!");
-                                }
-                                else
-                                {
-                                    HuidigeGebruiker = new Gebruiker(1, 1, regName, regEmail, regPhone, regPassword);
-                                    userAccess.AddUser(HuidigeGebruiker);
-                                }
-                            }
-                            else if (loginChoice == "0")
-                            {
-                                // Do nothing, just return to the main menu
-                            }
-                            else
-                            {
-                                Console.WriteLine("Ongeldige keuze. Druk op een toets om verder te gaan...");
-                                Console.ReadKey(true);
-                            }
-
+                            var user = inlogUI.Login();
+                            if (user != null) HuidigeGebruiker = user;
                         }
-                        else
+                        else if (loginChoice == "2")
                         {
-                            Console.WriteLine("U bent al ingelogd als: " + HuidigeGebruiker.Naam);
-                            Console.WriteLine("Druk op een toets om verder te gaan...");
-                            Console.ReadKey(true);
+                            var user = registratieUI.Registreer();
+                            if (user != null) HuidigeGebruiker = user;
                         }
-                        break;
                     }
+                    else
+                    {
+                        Console.WriteLine("U bent al ingelogd als: " + HuidigeGebruiker.Naam);
+                        Console.WriteLine("Druk op een toets om verder te gaan...");
+                        Console.ReadKey(true);
+                    }
+
+                    db.Close();
+                    break;
+                }
 
                 case "4":
+                {
+                    if (HuidigeGebruiker.ID == 0)
                     {
-                        if (HuidigeGebruiker.ID == 0)
-                        {
-                            Console.WriteLine("U moet eerst inloggen.");
-                            Console.ReadKey();
-                            break;
-                        }
-
-                        DatabaseContext db = new DatabaseContext();
-                        ReserveringAccess reserveringAccess = new ReserveringAccess(db);
-                        TafelAccess tafelAccess = new TafelAccess(db);
-                        TijdslotAccess tijdslotAccess = new TijdslotAccess(db);
-                        UserAccess userAccess = new UserAccess(db);
-
-                        ReservationLogic logic = new ReservationLogic(reserveringAccess, tafelAccess, tijdslotAccess, userAccess);
-
-                        ReserveringOverzichtUI overzichtUI = new ReserveringOverzichtUI(logic, HuidigeGebruiker);
-                        overzichtUI.ShowOverzicht();
-
-                        db.Close();
+                        Console.WriteLine("U moet eerst inloggen.");
+                        Console.ReadKey();
                         break;
                     }
+
+                    DatabaseContext db = new DatabaseContext();
+                    ReserveringAccess reserveringAccess = new ReserveringAccess(db);
+                    TafelAccess tafelAccess = new TafelAccess(db);
+                    TijdslotAccess tijdslotAccess = new TijdslotAccess(db);
+                    UserAccess userAccess = new UserAccess(db);
+
+                    ReservationLogic logic = new ReservationLogic(reserveringAccess, tafelAccess, tijdslotAccess, userAccess);
+
+                    ReserveringOverzichtUI overzichtUI = new ReserveringOverzichtUI(logic, HuidigeGebruiker);
+                    overzichtUI.ShowOverzicht();
+
+                    db.Close();
+                    break;
+                }
 
                 case "0":
                     running = false;
                     break;
+
                 default:
                     Console.WriteLine("Ongeldige keuze. Druk op een toets om verder te gaan...");
                     Console.ReadKey(true);
@@ -295,4 +166,3 @@ public static class Menu
         }
     }
 }
-
